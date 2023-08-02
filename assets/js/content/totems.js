b3210e482921f2fda48a04bae05e90b16a9952b8
Evgeny <kas0733@gmail.com>
2023-08-02 23:14:24 +0500

    1.19.0: Ускорить слив тотемов

--- assets/js/content/totems.js
+++ assets/js/content/totems.js
@@ -1,1 +1,27 @@
-document.querySelector("form")?.submit();
+const formEl = document.querySelector("form");
+const selectEl = formEl.querySelector("select[name=item]");
+const values = [...selectEl.options].slice(0, 50).map(item => item.value);
+
+const saleTotems = async () => {
+    showLoader();
+    for (const value of values) {
+        await fetch(formEl.action, {
+            method: 'POST',
+            redirect: 'manual',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            body: `item=${value}`
+        })
+    }
+    hideLoader();
+    refresh();
+}
+
+if (values.length) {
+    saleTotems()
+        .catch(e => {
+            console.error(e);
+            hideLoader();
+        });
+}
